1057e850f7852cd7e5b96bd2cfbdce20e6297c64
Rafael Ferreira <rafael@champria.gg>
2021-08-15 09:16:36 -0300

    added file structure

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raferrei <ferreirarox@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/14 12:16:15 by raferrei          #+#    #+#             */
+/*   Updated: 2021/08/14 12:21:50 by raferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+#endif
