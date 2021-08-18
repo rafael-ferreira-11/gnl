03995f6621ef0496a2b1116377516cc1f5fb550a
Rafael Ferreira <ferreira@asia.com>
2021-08-18 11:13:23 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <ferreira@asia.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:16:00 by raferrei          #+#    #+#             */
-/*   Updated: 2021/08/18 11:12:47 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/18 11:13:17 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,6 @@ char    *get_next_line(int fd)
         free(ret);
         return (0);
     }
-    ret[index] = '\0';
+    ret[index++] = '\0';
     return (ret);
 }
