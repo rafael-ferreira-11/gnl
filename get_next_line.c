8aa929b6ad990b0ae0fa742f33ce193d50408c12
Rafael Ferreira <ferreira@asia.com>
2021-08-24 10:33:33 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <ferreira@asia.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:16:00 by raferrei          #+#    #+#             */
-/*   Updated: 2021/08/24 10:32:23 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/24 10:33:27 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,11 +125,13 @@ char *get_next_line(int fd)
 	}
 	if (*ret == 0)
 	{
-		free(overflow);
+		if (overflow)
+			free(overflow);
 		free(ret);
 		return (0);
 	}
-	free(overflow);
+	if (overflow)
+		free(overflow);
 	free(value);
 	return (ret);
 }
