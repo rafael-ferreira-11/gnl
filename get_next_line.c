280957327a6864b4dba14ded4641c0a9619319c2
Rafael Ferreira <ferreira@asia.com>
2021-08-26 09:22:03 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <ferreira@asia.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:16:00 by raferrei          #+#    #+#             */
-/*   Updated: 2021/08/26 09:21:21 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/26 09:21:58 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,7 +113,7 @@ char	*get_next_line(int fd)
 		return (0);
 	while (read(fd, value, buffer) > 0)
 	{
-		printf("str %s \n", value);
+		//printf("str %s \n", value);
 		if(offset)
 		{
 			index = offset;
