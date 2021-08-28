3139492b82dcad6f51e1003eebb47e69a5fb5828
Rafael Ferreira <ferreira@asia.com>
2021-08-28 08:45:21 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <ferreira@asia.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:16:00 by raferrei          #+#    #+#             */
-/*   Updated: 2021/08/27 11:56:06 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/28 08:45:04 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,6 @@ char	*get_next_line(int fd)
 		trigger = 0;
 	}
 	value = calloc(BUFFER_SIZE, 1);
-	if (!value)
-		return (0);
 	while (read(fd, value, BUFFER_SIZE) > 0)
 	{
 		index = 0;
