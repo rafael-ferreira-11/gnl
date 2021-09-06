826c33127dad210b07a9bf346ba2bc1a6403cfae
Rafael Ferreira <ferreira@asia.com>
2021-09-06 17:54:23 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <ferreira@asia.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:16:00 by raferrei          #+#    #+#             */
-/*   Updated: 2021/09/06 17:52:42 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/06 17:54:16 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,8 @@ char	*get_next_line(int fd)
 	{
 		if (read_line(BUFFER_SIZE, value, &ret, &trigger) == 1)
 		{
-			free(value);
+			if(!trigger)
+				free(value);
 			return (ret);
 		}
 	}
