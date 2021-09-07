f267ba7ef087e4f3bce28f87ccdb44b673e9a2fc
Rafael Ferreira <ferreira@asia.com>
2021-09-06 22:24:35 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <ferreira@asia.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:16:00 by raferrei          #+#    #+#             */
-/*   Updated: 2021/09/06 22:23:09 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/06 22:24:30 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,11 @@ char	*get_next_line(int fd)
 		return (0);
 	*ret = 0;
 	if (read_reserve(BUFFER_SIZE, value, &ret, &trigger) == 1)
+	{
+		if (!trigger)
+			free(value);
 		return (ret);
+	}
 	trigger = 0;
 	value = ft_calloc(BUFFER_SIZE, 1);
 	if (!value)
